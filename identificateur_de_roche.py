4505f47ca8533a3ffc7f5d37f69f3f498ae18461
edouardooo <133405037+edouardooo@users.noreply.github.com>
2024-08-26 22:51:42 +0200

    Update identificateur_de_roche.py

--- identificateur_de_roche.py
+++ identificateur_de_roche.py
@@ -142,7 +142,7 @@ def main():
     st.write("**Roche Identifiée :**")
     st.write("La roche est",", ".join(result))
     st.write("By Edouard Azoulay")
-    st.image("https://raw.githubusercontent.com/edouardooo/Identificateur-de-roche/d7bcde8e3d60914e9f2810f21d3688ed910e2b87/plutonique%20granite.jpg", use_column_width=True)
+    st.image("https://raw.githubusercontent.com/edouardooo/Identificateur-de-roche/d7bcde8e3d60914e9f2810f21d3688ed910e2b87/"+"%20".join(result)+".jpg", use_column_width=True)
 
 if __name__ == "__main__":
     main()
