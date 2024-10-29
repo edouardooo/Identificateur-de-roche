03302e1ba8124ffbdf0578290296294fb5bc24fd
edouardooo <133405037+edouardooo@users.noreply.github.com>
2024-10-29 16:59:29 +0100

    Update identificateur_de_roche.py

--- identificateur_de_roche.py
+++ identificateur_de_roche.py
@@ -2,6 +2,12 @@ import streamlit as st
 from streamlit_javascript import st_javascript
 from fonctions_d_identification import identificationsedimentaire, identificationmagmatique, identificationmetamorphique
 
+try:
+    from fonctions_d_identification import identificationsedimentaire, identificationmagmatique, identificationmetamorphique
+    print("Modules importés avec succès")
+except ModuleNotFoundError as e:
+    print("Erreur d'importation :", e)
+    
 def afficher_carte():
     st.header("La carte des kayous")
     st.write("Voici la carte des affleurements et des échantillons.")
