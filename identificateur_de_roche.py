3dcf7e5843616dbee8a735185c797c697a9d49a7
edouardooo <133405037+edouardooo@users.noreply.github.com>
2024-10-30 15:41:19 +0100

    Update identificateur_de_roche.py

--- identificateur_de_roche.py
+++ identificateur_de_roche.py
@@ -24,7 +24,7 @@ def afficher_carte():
         <div class="iframe-container">
             <iframe src="{map_url}" class="iframe"></iframe>
         </div>
-    """, height=600)
+    """, height=700)
     st.write("By Edouard Azoulay")
 
 
