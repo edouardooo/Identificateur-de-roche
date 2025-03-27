a2bb11bd861b92da6672e6e211564acd9d73e522
edouardooo <133405037+edouardooo@users.noreply.github.com>
2025-03-27 11:24:55 +0100

    Update upload_image.py

--- upload_image.py
+++ upload_image.py
@@ -22,24 +22,23 @@ def uploader_image():
             st.image(uploaded_file, caption="Image en cours de chargement", width=150)
         
         with col2:
-            # Affichage du lien direct sous le titre
-            st.subheader("Voici le lien direct de votre image:")
             # Upload image à Cloudinary
             response = cloudinary.uploader.upload(uploaded_file)
             
             # Récupérer l'URL de l'image
             image_url = response['secure_url']
             
-            # Affichage du lien dans une zone de texte (lecture seule) avec échappement des accolades
-            st.text_area("Lien direct", value=f"{{{{ {image_url} }}}}", height=50)
-
+            # Affichage du lien avec des accolades dans une zone markdown
+            st.subheader("Voici le lien direct de votre image:")
+            st.markdown(f"**Lien direct:** {{ {image_url} }}")
+            
             # Ajout du bouton pour copier le lien
             copy_button = st.button('Copier le lien')
             
             if copy_button:
                 # Injecter du JS pour copier le texte dans le presse-papiers
                 st_javascript(f"""
-                const textToCopy = '{{{{{image_url}}}}}';
+                const textToCopy = '{{ {image_url} }}';
                 navigator.clipboard.writeText(textToCopy).then(function() {{
                     alert('Lien copié dans le presse-papiers !');
                 }}).catch(function(err) {{
@@ -50,4 +49,3 @@ def uploader_image():
         st.success("Image uploadée avec succès !")
     else:
         st.warning("Veuillez télécharger une image.")
-
