70f7833b36fc8eec6ad97cdf27117185354282a6
edouardooo <133405037+edouardooo@users.noreply.github.com>
2025-03-27 10:46:40 +0100

    Update upload_image.py

--- upload_image.py
+++ upload_image.py
@@ -15,21 +15,22 @@ def uploader_image():
     uploaded_file = st.file_uploader("Choisissez une image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file:
-        # Affichage de l'image en petit pendant le chargement
-        st.image(uploaded_file, caption="Image en cours de chargement", width=150)
+        # Création de 2 colonnes pour afficher l'image et le lien côte à côte
+        col1, col2 = st.columns([2, 3])  # Ajuste la largeur des colonnes si nécessaire
         
-        # Envoi à Cloudinary
-        st.write("Envoi en cours...")
+        with col1:
+            # Affichage de l'image en petit pendant le chargement
+            st.image(uploaded_file, caption="Image en cours de chargement", width=150)
         
-        # Upload image à Cloudinary
-        response = cloudinary.uploader.upload(uploaded_file)
-        
-        # Récupérer l'URL de l'image
-        image_url = response['secure_url']
-        
-        # Affichage du lien direct de l'image sous le titre
-        st.subheader("Voici le lien direct de votre image:")
-        st.write(f"{{{{ {image_url} }}}}")  # Affichage dans le format demandé
+        with col2:
+            # Affichage du lien direct sous le titre
+            st.subheader("Voici le lien direct de votre image:")
+            # Upload image à Cloudinary
+            response = cloudinary.uploader.upload(uploaded_file)
+            
+            # Récupérer l'URL de l'image
+            image_url = response['secure_url']
+            st.write(f"{{{{ {image_url} }}}}")  # Affichage dans le format demandé
         
         st.success("Image uploadée avec succès !")
     else:
